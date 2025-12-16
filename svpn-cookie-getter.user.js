000ef4ce64d0280d9f178a429b3dd61637cc13c0
Tess Hoffman <tess@zrhoffman.net>
2025-12-16 15:13:47 -0700

    More streamlined syntax in userscript.

--- svpn-cookie-getter.user.js
+++ svpn-cookie-getter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SVPN cookie getter
 // @namespace    SVPN cookie getter
-// @version      0.1
+// @version      0.2
 // @description  Gets the cookie to use with svpn-login
 // @match        https://*.vpn.comcast.net/vdesk/webtop.eui*
 // @grant        none
@@ -9,8 +9,8 @@
 
 (function() {
     const resourceType = "network_access";
-    with (new XMLHttpRequest()) {
-        open("GET", `https://${location.host}:${location.port}/vdesk/resource_list.xml?resourcetype=res`);
+    with (new XMLHttpRequest()) with (location) {
+        open("GET", `https://${host}:${port}/vdesk/resource_list.xml?resourcetype=res`);
         onload = () => document.documentElement.innerHTML = `<pre>${document.cookie.match(/MRHSession=(.*?); /)[1]}</pre>`;
         send();
     }
